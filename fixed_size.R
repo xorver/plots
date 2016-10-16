95fb0e2e8076dd31e968b51f6a405c64d9d0a312
Tomasz Lichon <t.j.lichon@gmail.com>
2016-10-17 00:11:02 +0200

    Add 16GB data.

--- fixed_size.R
+++ fixed_size.R
@@ -23,30 +23,38 @@ pdf(args[2])
 # plot 1
 TH = Throughput[1:ChunkNum]
 SD = ThroughputSD[1:ChunkNum]
-plot(UniqueChunk, Throughput[1:ChunkNum], ylim=c(250, 400), xlab="Chunk Size [MB]", col='blue', xaxt='n', log="x", ylab="Throughput [MB / s]")
+plot(UniqueChunk, TH, ylim=c(200, 400), xlab="Chunk Size [MB]", col='green', xaxt='n', log="x", ylab="Throughput [MB / s]")
 axis(1, at=UniqueChunk, lab=c(UniqueChunk[1:(ChunkNum-1)], "fixed"))
-#arrows(UniqueChunk, TH-SD, UniqueChunk, TH+SD, col='blue', length=0.05, angle=90, code=3)
+#arrows(UniqueChunk, TH-SD, UniqueChunk, TH+SD, col='green', length=0.05, angle=90, code=3)
 P <- predict(loess(TH ~ UniqueChunk))
-lines(P ~ UniqueChunk, col="blue", lwd=1, lty='dashed')
+lines(P ~ UniqueChunk, col="green", lwd=1, lty='dashed')
 
 # 2nd file
 TH = Throughput[(ChunkNum+1):(2*ChunkNum)]
 SD = ThroughputSD[(ChunkNum+1):(2*ChunkNum)]
-points(UniqueChunk, Throughput[(ChunkNum+1):(2*ChunkNum)], pch=22, lty=2, col="red")
+points(UniqueChunk, TH, pch=22, lty=2, col="blue")
+#arrows(UniqueChunk, TH-SD, UniqueChunk, TH+SD, col='blue', length=0.05, angle=90, code=3)
+P <- predict(loess(TH ~ UniqueChunk))
+lines(P ~ UniqueChunk, col="blue", lwd=1, lty='dashed')
+
+# 3rd file
+TH = Throughput[(2*ChunkNum+1):(3*ChunkNum)]
+SD = ThroughputSD[(2*ChunkNum+1):(3*ChunkNum)]
+points(UniqueChunk, TH, pch=23, lty=3, col="red")
 #arrows(UniqueChunk, TH-SD, UniqueChunk, TH+SD, col='red', length=0.05, angle=90, code=3)
 P <- predict(loess(TH ~ UniqueChunk))
 lines(P ~ UniqueChunk, col="red", lwd=1, lty='dashed')
 
 # 3rd file
-TH = Throughput[(2*ChunkNum+1):(3*ChunkNum)]
-SD = ThroughputSD[(2*ChunkNum+1):(3*ChunkNum)]
-points(UniqueChunk, Throughput[(2*ChunkNum+1):(3*ChunkNum)], pch=23, lty=2, col="black")
+TH = Throughput[(3*ChunkNum+1):(4*ChunkNum)]
+SD = ThroughputSD[(3*ChunkNum+1):(4*ChunkNum)]
+points(UniqueChunk, TH, pch=24, lty=4, col="black")
 #arrows(UniqueChunk, TH-SD, UniqueChunk, TH+SD, col='black', length=0.05, angle=90, code=3)
 P <- predict(loess(TH ~ UniqueChunk))
 lines(P ~ UniqueChunk, col="black", lwd=1, lty='dashed')
 
 # legend
-legend(17, 270, c("2GB File", "4GB File", "6GB File"), cex=0.8,
-col=c("blue", "red", "black"), pch=21:23, lty=1:3);
+legend(17, 230, c("2GB File", "4GB File", "8GB File", "16GB File"), cex=0.8,
+col=c("green", "blue", "red", "black"), pch=21:24, lty=1:4);
 
 dev.off()
