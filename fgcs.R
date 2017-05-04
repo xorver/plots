6baa08fd8a3032cd3317edaa9a58d4368c51cb40
Tomasz Lichon <t.j.lichon@gmail.com>
2017-05-04 23:07:44 +0200

    Add fgcs plots.

---
+++ fgcs.R
@@ -0,0 +1,57 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+options(scipen=5)
+
+SharedRead = read.csv(args[1], header = TRUE, sep = ",")
+SeparatedRead = read.csv(args[2], header = TRUE, sep = ",")
+SharedWrite = read.csv(args[3], header = TRUE, sep = ",")
+SeparatedWrite = read.csv(args[4], header = TRUE, sep = ",")
+
+SharedReadProviderId = aggregate(SharedRead$Throughput ~ SharedRead$ProviderId, FUN=sd)$'SharedRead$ProviderId'
+SharedReadThroughputSd = aggregate(SharedRead$Throughput ~ SharedRead$ProviderId, FUN=sd)$'SharedRead$Throughput'
+SharedReadThroughput = aggregate(SharedRead$Throughput ~ SharedRead$ProviderId, FUN=mean)$'SharedRead$Throughput'
+
+SeparatedReadProviderId = aggregate(SeparatedRead$Throughput ~ SeparatedRead$ProviderId, FUN=sd)$'SeparatedRead$ProviderId'
+SeparatedReadThroughputSd = aggregate(SeparatedRead$Throughput ~ SeparatedRead$ProviderId, FUN=sd)$'SeparatedRead$Throughput'
+SeparatedReadThroughput = aggregate(SeparatedRead$Throughput ~ SeparatedRead$ProviderId, FUN=mean)$'SeparatedRead$Throughput'
+
+SharedWriteProviderId = aggregate(SharedWrite$Throughput ~ SharedWrite$ProviderId, FUN=sd)$'SharedWrite$ProviderId'
+SharedWriteThroughputSd = aggregate(SharedWrite$Throughput ~ SharedWrite$ProviderId, FUN=sd)$'SharedWrite$Throughput'
+SharedWriteThroughput = aggregate(SharedWrite$Throughput ~ SharedWrite$ProviderId, FUN=mean)$'SharedWrite$Throughput'
+
+SeparatedWriteProviderId = aggregate(SeparatedWrite$Throughput ~ SeparatedWrite$ProviderId, FUN=sd)$'SeparatedWrite$ProviderId'
+SeparatedWriteThroughputSd = aggregate(SeparatedWrite$Throughput ~ SeparatedWrite$ProviderId, FUN=sd)$'SeparatedWrite$Throughput'
+SeparatedWriteThroughput = aggregate(SeparatedWrite$Throughput ~ SeparatedWrite$ProviderId, FUN=mean)$'SeparatedWrite$Throughput'
+
+pdf(args[5])
+
+# plot 1
+plot(SharedReadProviderId, SharedReadThroughput, pch=1, ylim=c(0, 1000), xlab="Number of providers", col='black', xaxt='n', ylab="Throughput [MB / s]")
+axis(1, at=SharedReadProviderId, lab=SharedReadProviderId)
+
+# shared
+arrows(SharedReadProviderId, SharedReadThroughput-SharedReadThroughputSd, SharedReadProviderId, SharedReadThroughput+SharedReadThroughputSd, col='black', length=0.05, angle=90, code=3)
+P <- predict(loess(SharedReadThroughput ~ SharedReadProviderId))
+lines(P ~ SharedReadProviderId, col="black", lwd=1, lty='dashed')
+
+# separated
+points(SeparatedReadProviderId, SeparatedReadThroughput, pch=0, lty=2, col="red")
+arrows(SeparatedReadProviderId, SeparatedReadThroughput-SeparatedReadThroughputSd, SeparatedReadProviderId, SeparatedReadThroughput+SeparatedReadThroughputSd, col='red', length=0.05, angle=90, code=3)
+P <- predict(loess(SeparatedReadThroughput ~ SeparatedReadProviderId))
+lines(P ~ SeparatedReadProviderId, col="red", lwd=1, lty='dashed')
+
+points(SharedWriteProviderId, SharedWriteThroughput, pch=2, lty=2, col="blue")
+arrows(SharedWriteProviderId, SharedWriteThroughput-SharedWriteThroughputSd, SharedWriteProviderId, SharedWriteThroughput+SharedWriteThroughputSd, col='blue', length=0.05, angle=90, code=3)
+P <- predict(loess(SharedWriteThroughput ~ SharedWriteProviderId))
+lines(P ~ SharedWriteProviderId, col='blue', lwd=1, lty='dashed')
+
+points(SeparatedWriteProviderId, SeparatedWriteThroughput, pch=5, lty=2, col="green")
+arrows(SeparatedWriteProviderId, SeparatedWriteThroughput-SeparatedWriteThroughputSd, SeparatedWriteProviderId, SeparatedWriteThroughput+SeparatedWriteThroughputSd, col='green', length=0.05, angle=90, code=3)
+P <- predict(loess(SeparatedWriteThroughput ~ SeparatedWriteProviderId))
+lines(P ~ SeparatedWriteProviderId, col='green', lwd=1, lty='dashed')
+
+# legend
+legend("bottomright", legend=c("Shared space read","Local space read", "Shared space write", "Local space write"), cex=0.8, col=c("black","red","blue", "green"), pch=c(1, 0, 2, 5), lty=c(2,2,2,2));
+
+dev.off()
---
+++ fgcs_plot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./fgcs.R fgcs_data/shared_space_filtered_read.csv fgcs_data/separated_space_filtered_read.csv \
+  fgcs_data/shared_space_filtered_write.csv fgcs_data/separated_space_filtered_write.csv \
+ results/fgcs.pdf && open results/fgcs.pdf
