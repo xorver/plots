d0e9a44ee50d16f6cfa6230a0fb7fd1a02dc61b2
Tomasz Lichon <t.j.lichon@gmail.com>
2017-09-02 13:23:15 +0200

    Rename 'fgcs' plots to 'multiprovider_sync_overhead'.

--- multiprovider_sync_overhead.sh
+++ multiprovider_sync_overhead.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./fgcs.R fgcs_data/shared_space_filtered_read.csv \
+./multiprovider_sync_overhead.R fgcs_data/shared_space_filtered_read.csv \
   fgcs_data/separated_space_filtered_read.csv \
   fgcs_data/shared_space_filtered_write.csv \
   fgcs_data/separated_space_filtered_write.csv \
   fgcs_data/shared_space_first_read.csv \
-  results/fgcs.pdf && open results/fgcs.pdf
+  results/multiprovider_sync_overhead.pdf && open results/multiprovider_sync_overhead.pdf
